bd81e83536dec15bbe54dea4d5810044bf16bf7d
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-07-04 17:01:33 +0200

    detesting and de-here'ing

--- test/browser-resources.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2023-     XYZ maps contributors
- * Copyright (C) 2017-2020 HERE Europe B.V.
- * Licensed under Apache 2.0, see full license in LICENSE
- * SPDX-License-Identifier: Apache-2.0
- */
-
-window.TEST_RESOURCES_DIR = "/browser/";
