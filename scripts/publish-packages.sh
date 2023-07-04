3af04c2005360abcd63e2a321e3016c263cde430
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-07-04 17:39:21 +0200

    detesting and de-here'ing

--- scripts/publish-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-#
-# Simple script that bundles the publishing of packages to be run from CI
-#
-
-echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
-npx lerna publish -y from-git --pre-dist-tag alpha
